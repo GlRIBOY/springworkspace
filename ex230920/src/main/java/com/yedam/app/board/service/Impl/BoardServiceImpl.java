7a78bbe2cc9c28b38b5fbf07ad06c91692424e10
GlRIBOY <wkd3795@hanmail.net>
2023-09-20 23:17:59 +0900

    230920과제

--- ex230920/src/main/java/com/yedam/app/board/service/Impl/BoardServiceImpl.java
+++ ex230920/src/main/java/com/yedam/app/board/service/Impl/BoardServiceImpl.java
@@ -2,39 +2,51 @@ package com.yedam.app.board.service.Impl;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.yedam.app.board.mapper.BoardMapper;
 import com.yedam.app.board.service.BoardService;
 import com.yedam.app.board.service.BoardVO;
+
 @Service
 public class BoardServiceImpl implements BoardService {
 
-@Autowired 
-BoardMapper boardMapper;
-@Override
+	@Autowired
+	BoardMapper boardMapper;
+
+	@Override
 	public List<BoardVO> getBoardList() {
-List<BoardVO> list = boardMapper.selectBoardList(new BoardVO);
+		List<BoardVO> list = boardMapper.selectBoardList();
 		return list;
 	}
 
 	@Override
 	public BoardVO getBoardInfo(BoardVO boardVO) {
-		return null;
+		return boardMapper.selectBoardInfo(boardVO);
 	}
 
 	@Override
 	public int insertBoardInfo(BoardVO boardVO) {
-		return 0;
+		if (boardMapper.insertBoard(boardVO) == 1) {
+			return Integer.parseInt(boardVO.getBno());
+		} else {
+			return -1;
+		}
 	}
 
 	@Override
 	public int updateBoardInfo(BoardVO boardVO) {
-		return 0;
+		if (boardMapper.updateBoard(boardVO) == 1) {
+			return Integer.parseInt(boardVO.getBno());
+		} else {
+			return -1;
+		}
 	}
 
 	@Override
 	public int deleteBoardInfo(int boardNo) {
-		return 0;
+		return boardMapper.deleteBoard(boardNo);
 	}
-	
+
 }
